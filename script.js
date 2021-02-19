6cc9f9d20f3ce4b65946caae4495ee899631ebdb
michaeltheboyer <michael.boyer@ocsbstudent.ca>
2021-02-19 17:49:10 +0000

    Third times a charm

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 document.getElementById('button').addEventListener('click', mathstuff)
 
 function mathstuff () {
-  //Area first
-  const diameter = document.getElementById('userinput').value //input
-  let radius = parseFloat(diameter / 2)
-  let area = Math.PI * (radius * radius)
-  let circumference = Math.PI * diameter
+  const diameter = document.getElementById('userinput').value
+  const radius = parseFloat(diameter / 2)
+  const area = Math.PI * (radius * radius)
+  const circumference = Math.PI * diameter
   document.getElementById('answer').innerHTML = ' Area is ' + area + '<br> Circumference is ' + circumference
 }

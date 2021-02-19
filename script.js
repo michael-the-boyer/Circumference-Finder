6993ac36a23584fffb656160e56fdd009926dea4
michaeltheboyer <michael.boyer@ocsbstudent.ca>
2021-02-19 17:45:58 +0000

    Single quotationmarks afsdskihf

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ function mathstuff () {
   let radius = parseFloat(diameter / 2)
   let area = Math.PI * (radius * radius)
   let circumference = Math.PI * diameter
-  document.getElementById('answer').innerHTML = ' Area is ' + area + "<br> Circumference is " + circumference
+  document.getElementById('answer').innerHTML = ' Area is ' + area + '<br> Circumference is ' + circumference
 }
